a853474e7bec58908f657e4997dabb32042e1a17
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-01 12:34:29 -0500

    added timestmap on for latest demos under the FEE org, could not figure out how to make click event work

--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-page.getRepos();
-page.displayProfilePicture();
+page.init();
 });//end of doc ready
 var page = {
+
+url: 'https://api.github.com/users/baileyhood',
+
+init: function () {
+  page.getRepos();
+  page.getFEEDemos();
+  page.displayProfilePicture();
+},
+
+events: function() {
+    $('h5').on('click', function(event) {
+    alert("I was clicked!");
+    });
+},
 
 sortRepos: function(object) { //sorting by date
       return _.sortBy(object, 'updated_at').reverse();
@@ -11,7 +24,7 @@ sortRepos: function(object) { //sorting by date
 postRepo: function (object) {
     var emptyArr = '';
     _.each(page.sortRepos(object), function(el) {
-      emptyArr += '<a href=' + el.html_url +'<h3 class = "reponame">' + "Repo: " + el.name + '</h3> </a>';
+      emptyArr += '<a href=' + el.html_url +'<h3 class = "reponame">' + el.name + '</h3> </a>';
       emptyArr += '<p class = description>'  + el.description + '</p>';
       emptyArr += "<p>" + moment(el.created_at,"YYYYMMDDH").fromNow() + "</p>";
     });
@@ -28,12 +41,25 @@ getRepos: function () {
   });
 },
 
+getFEEDemos: function () {
+  $.ajax({
+    url: 'https://api.github.com/repos/TIY-Charleston-Front-End-February2016/demos',
+    method: 'GET',
+    success: function (time) {
+      var timeNow = moment(time.pushed_at,"YYYYMMDDH").fromNow();
+      $('h6').append(timeNow);
+
+    }
+    });
+},
+
 displayProfilePicture: function () {
   $.ajax({
     url: 'https://api.github.com/users/baileyhood',
     method: 'GET',
     success: function (object) {
-      console.log (object.avatar_url);
+      $('h1').append(object.name);
+      $('h2').append("Username: " + object.login);
       $('#profile-pic').attr("src", object.avatar_url);
     }
 });
